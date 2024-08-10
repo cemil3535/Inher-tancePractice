fef7981d29e93a96865b9df079c91d1f4e3ef929
cemil3535 <cemilozcan35@gmail.com>
2024-08-11 00:04:31 +0300

    Add project files.

---
+++ InherıtancePractice/BaseKisi.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InherıtancePractice
+{
+    public abstract class BaseKisi
+    {
+        // property to identify
+        public string Name {  get; set; }
+        public string Surname {  get; set; }
+
+        // name and surnamae metod to identify
+        public void DisplayNameSurname(string name, string surname)
+        {
+            Console.Write($"Ad ve Soyad: {name} {surname}");
+        }
+
+        // derived student class.
+        public class Student : BaseKisi
+        {
+            public string StudentNumber {  get; set; }
+
+            // teacher show information metod
+            public  void DisplayStudentInfo(string studentName, string studentSurname, string studentNumber)
+            {
+                DisplayNameSurname(studentName, studentSurname);
+
+                Console.WriteLine($" Ogrencinin Numarasi: {studentNumber}");
+            } 
+
+        }
+
+        // derived reacher class
+        public class Teacher : BaseKisi
+        {
+            public decimal Salary { get; set; }
+
+            // teacher show information metod
+            public void DisplayTeacherInfo(string teacherName, string teacherSurname, decimal salary)
+            {
+                DisplayNameSurname(teacherName, teacherSurname);
+                salary = Convert.ToDecimal(salary);
+                Console.WriteLine($" Ogretmen maasi {salary} TL.dir.");
+            }
+
+        }
+
+        
+    }
+}
---
+++ InherıtancePractice/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using InherıtancePractice;
+using Microsoft.VisualBasic;
+using System.Diagnostics.Metrics;
+using static InherıtancePractice.BaseKisi;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        // Student create object and  want to information by metod
+        Student student = new Student();
+
+        Console.Write("Lutfen adinizi giriniz: ");
+        string name = Console.ReadLine();
+        Console.Write("lutfen soyadinizi giriniz: ");
+        string surname = Console.ReadLine();
+        Console.Write("Lutfen Ogreni Numarasini Giriniz: ");
+        string number = Console.ReadLine();
+        student.DisplayStudentInfo(name, surname, number);
+
+
+        // Teacher create object and  want to information by metod
+        Teacher teacher = new Teacher();
+        Console.Write("Lutfen adinizi giriniz: ");
+        string teacherName = Console.ReadLine();
+        Console.Write("lutfen soyadinizi giriniz: ");
+        string teacherSurname = Console.ReadLine();
+        Console.Write("Lutfen Aylik Maasinizi Giriniz: ");
+        decimal salary = Convert.ToDecimal(Console.ReadLine());
+        teacher.DisplayTeacherInfo(teacherName,teacherSurname, salary);
+
+
+    }
+
+
+}
